b669711be6c36af6b53bcb2677ed3a50944bba1f
Daniel Kelly <danielkelly@randallreilly.com>
2020-12-22 15:15:57 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Nuxt Content Algolia Module
+Automatically (during `npm run generate`) sync content stored in your project with nuxt content`to an [Algolia](https://www.algolia.com) index. This allows you to manage your content in your repo while providing powerful search capabilities to your site users.
+
+Works great with sites hosted on Netlify, etc that automatically run your build command when you commit new content.
+
+## Installation
+```
+npm install nuxt-content-algolia algoliasearch
+```
+
+## Quickstart
+```javascript
+// nuxt.config.js
+export default{
+    buildModules: [
+        'nuxt-content-algolia',
+        // ...
+    ],
+    nuxtContentAlgolia: {
+        appId: process.env.ALGOLIA_APP_ID,
+        // !IMPORTANT secret key should always be an environment variable
+        // this is not your search only key but the key that grants access to modify the index
+        apiKey: process.env.ALGOLIA_API_KEY, 
+        
+        // relative to content directory
+        // each path get's its own index
+        paths: [
+            {
+                name: 'blog',
+                // optional (will use name if index not specified)
+                index: process.env.ALGOLIA_INDEX || 'blog',
+                fields: ['title', 'description', 'bodyPlainText', 'tags'],
+            },
+        ],
+    },
+}
+```
---
+++ module.js
@@ -0,0 +1,44 @@
+const consola = require('consola')
+const algoliasearch = require('algoliasearch')
+
+export default function algoliaModule(moduleOptions = {}) {
+    this.options.nuxtAlgolia = this.options.nuxtContentAlgolia || {}
+    const config = {
+        hook: 'generate:done',
+        ...this.options.nuxtAlgolia,
+        ...moduleOptions,
+    }
+    
+    if (!config.appId || !config.apiKey) {
+        consola.error(
+            new Error('appId and apiKey are required for nuxt-algolia module')
+        )
+        return
+    }
+    
+    this.nuxt.hook(config.hook, async (nuxt) => {
+        const { $content } = require(`${nuxt.options.srcDir}/node_modules/@nuxt/content`)
+        for (let i = 0; i < config.paths.length; i++) {
+            const path = config.paths[i]
+            const indexName = path.index || path.name
+            let docs = await $content(path.name).fetch()
+            
+            docs = docs.map((doc) => {
+                const newDoc = {}
+                path.fields.forEach((field) => (newDoc[field] = doc[field]))
+                newDoc.objectID = doc.slug
+                return newDoc
+            })
+            
+            const client = algoliasearch(config.appId, config.apiKey)
+            const index = client.initIndex(indexName)
+            
+            const { objectIDs } = await index.saveObjects(docs)
+            consola.success(
+                `Indexed ${objectIDs.length} records in Algolia for: ${indexName}`
+            )
+        }
+    })
+}
+
+module.exports.meta = require('./package.json')
